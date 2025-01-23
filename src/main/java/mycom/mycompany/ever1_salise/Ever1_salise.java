35dc6f02222b55ee9553f0af03f5916fad110c2e
Kenji Sebastian Salise <kenjimsalise@gmail.com>
2025-01-23 19:24:37 +0900

    hello brand new world

---
+++ src/main/java/mycom/mycompany/ever1_salise/Ever1_salise.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mycom.mycompany.ever1_salise;
+
+/**
+ *
+ * @author picoh
+ */
+public class Ever1_salise {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("hello = " + hello);
+}
